85248bc0939ccced4f084ac943498e62f817aa07
Kebabsavar31 <147258058+kebabistik@users.noreply.github.com>
2025-02-02 19:40:41 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+# Kullanıcıdan bir metin al
+text = input("Bir metin girin: ")
+
+# Metni tersine çevir
+reversed_text = text[::-1]
+
+# Sonucu ekrana yazdır
+print("Ters çevrilmiş metin:", reversed_text)
